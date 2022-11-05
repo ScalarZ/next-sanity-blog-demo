3024b3cb7024f04a09940dbe500d1e389bcee46a
faresrahali44@gmail.com <faresrahali44@gmail.com>
2022-11-05 09:07:25 +0100

    next-sanity-blog

---
+++ components/Post.tsx
@@ -0,0 +1,48 @@
+import Image from "next/legacy/image";
+import { urlFor } from "../lib/sanity";
+
+const myLoader = ({ src }: { src: string }) => {
+  return src;
+};
+
+interface Props {
+  _ref: string;
+  title: string;
+  categories: Array<any>;
+  author: string;
+  _createdAt: string;
+}
+
+const Post = ({ _ref, _createdAt, title, categories, author }: Props) => {
+  return (
+    <div className="overflow-hidden rounded-lg bg-white text-black cursor-pointer">
+      <div className="relative h-60">
+        <Image
+          loader={myLoader}
+          src={urlFor(_ref).url()}
+          blurDataURL={urlFor(_ref).blur(10).url()}
+          alt="#"
+          layout="fill"
+        />
+      </div>
+      <div className="p-2">
+        <h2 className="text-2xl font-semibold">{title}</h2>
+        <ul className="flex gap-x-2 text-sm opacity-70">
+          {categories.map((category, index) => (
+            <li>
+              {`${category.title} ${
+                index < categories.length - 1 ? " / " : ""
+              }`}
+            </li>
+          ))}
+        </ul>
+        <h3 className="font-medium">By: &nbsp;{author}</h3>
+        <span className="text-xs">
+          Posted: {new Date(_createdAt).toDateString()}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
---
+++ pages/post/[id].tsx
@@ -0,0 +1,127 @@
+import Image from "next/legacy/image";
+
+const myLoader = ({ src }: { src: string }) => {
+  return src;
+};
+
+const client = createClient({
+  projectId: "dhs8i92d",
+  dataset: "production",
+  apiVersion: "2022-11-04",
+  useCdn: false,
+});
+
+interface Props {
+  post: any;
+}
+
+const Blog = ({ post }: Props) => {
+  console.log(post);
+  return (
+    <div className="p-4 min-h-screen bg-gray-900 text-white">
+      <header className="relative h-56">
+        <Image
+          loader={myLoader}
+          src={urlFor(post?.mainImage.asset._ref).url()}
+          blurDataURL={urlFor(post?.mainImage.asset._ref).blur(10).url()}
+          alt="#"
+          layout="fill"
+          objectFit="cover"
+        />
+      </header>
+      <h1 className="mt-6 mb-2 text-red-500 font-bold text-4xl">
+        {post?.title}
+      </h1>
+      <ul className="mb-6 flex gap-x-2 text-sm opacity-70">
+        <li>
+          <b>Category:</b>
+        </li>
+        {post?.categories.map((category: any) => (
+          <li>{category.title}</li>
+        ))}
+      </ul>
+      <main>
+        {post.body.map((block: any) =>
+          block.children ? (
+            <p key={block._key}>{block.children[0].text}</p>
+          ) : block.asset ? (
+            <Image
+              loader={myLoader}
+              src={urlFor(block?.asset._ref).url()}
+              blurDataURL={urlFor(block?.asset._ref).blur(10).url()}
+              alt="#"
+              height={200}
+              width={300}
+              objectFit="contain"
+            />
+          ) : null
+        )}
+      </main>
+      <footer className="mt-6 text-xl text-green-400">
+        <span className="flex items-center">
+          <span className="mr-2 flex justify-center items-center rounded-full border-2 border-green-400">
+            <Image
+              loader={myLoader}
+              src={urlFor(post?.author.image.asset._ref).url()}
+              alt="#"
+              height={50}
+              width={50}
+              objectFit="cover"
+              className="rounded-full"
+            />
+          </span>
+          Author: &nbsp; <b>{post?.author.name}</b>
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+import { GetStaticPaths, GetStaticProps } from "next";
+import { createClient } from "next-sanity";
+import { urlFor } from "../../lib/sanity";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = (await client.fetch(
+    `*[_type == "post"]{slug{current}}`
+  )) as Array<any>;
+  const paths = slugs.map(({ slug }) => ({ params: { id: slug.current } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const post = (await client.fetch(
+    `*[_type == "post" && slug.current == "${params?.id}"]{
+        title,
+        body,
+        mainImage{
+         asset{_ref}
+        },
+        author->{
+          name,
+          image{
+            asset{_ref}
+           },
+        },
+        categories[]->{title}
+      }`
+  )) as Array<any>;
+
+  if (!post[0]) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      post: post[0],
+    },
+  };
+};
+export default Blog;
